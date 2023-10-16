f0785591acdde489d0f4b46cd8da92bbdc696a80
yumekiti <yumekiti120440@gmail.com>
2023-10-17 04:44:26 +0900

    fix: cms dockerfile

--- push.sh
+++ push.sh
@@ -2,16 +2,16 @@
 
 docker_build_push() {
     cd $1
-    docker build --platform linux/arm64 -t yumekiti/$2 .
+    docker build --platform $3 -t yumekiti/$2 .
     docker push yumekiti/$2
     cd ..
 }
 
 # api
-docker_build_push "api" "cocoroiki-api"
+docker_build_push "api" "cocoroiki-api" "linux/arm64"
 
 # bff
-docker_build_push "bff" "cocoroiki-bff"
+docker_build_push "bff" "cocoroiki-bff" "linux/arm64"
 
 # cms
-docker_build_push "cms" "cocoroiki-cms"
+docker_build_push "cms" "cocoroiki-cms" "linux/arm64/v8"
